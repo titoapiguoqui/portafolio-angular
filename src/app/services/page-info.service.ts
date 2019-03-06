acc056406af235a234c5ff05205ac20bd98f993f
Héctor Martínez <h.martinez@gobalo.es>
2019-03-06 15:27:12 +0100

    Firebase automation team

--- src/app/services/page-info.service.ts
+++ src/app/services/page-info.service.ts
@@ -9,12 +9,28 @@ export class PageInfoService {
   info: PageInfo = {};
   loaded = false;
 
+  team: any[] = [];
+
   constructor( private http: HttpClient) {
+    this.loadInfo();
+    this.loadTeam();
+  }
+
+  private loadInfo() {
     // Read JSON file
     this.http.get('assets/data/page-data.json')
              .subscribe( (resp: PageInfo) => {
                this.info = resp;
                this.loaded = true;
+
+               console.log( resp );
+             });
+  }
+
+  private loadTeam() {
+    this.http.get('https://angular-html-efe8c.firebaseio.com/team.json')
+             .subscribe( (resp: any) => {
+               this.team = resp;
 
                console.log( resp );
              });
